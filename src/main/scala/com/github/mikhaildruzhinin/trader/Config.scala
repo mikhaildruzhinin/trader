de528bc4d78cf335542290b8c8d49c5bcf227803
Mikhail Druzhinin <druzhininmy@gmail.com>
2023-05-17 21:14:27 +0300

    add rate limit pause

--- src/main/scala/com/github/mikhaildruzhinin/trader/Config.scala
+++ src/main/scala/com/github/mikhaildruzhinin/trader/Config.scala
@@ -1,9 +1,12 @@
 package com.github.mikhaildruzhinin.trader
 
-case class Config(tinkoffInvestApiToken: String,
+case class Config(tinkoffInvestApi: TinkoffInvestApi,
                   exchange: String,
                   pctScale: Int,
                   priceScale: Int,
                   uptrendThresholdPct: Int,
                   numUptrendShares: Int,
                   incomeTaxPct: Int)
+
+case class TinkoffInvestApi(token: String,
+                            rateLimitPauseMillis: Long)
--- src/main/scala/com/github/mikhaildruzhinin/trader/util.scala
+++ src/main/scala/com/github/mikhaildruzhinin/trader/util.scala
@@ -1,20 +1,40 @@
 package com.github.mikhaildruzhinin.trader
 
+import com.typesafe.scalalogging.Logger
 import ru.tinkoff.piapi.contract.v1.{CandleInterval, HistoricCandle}
 import ru.tinkoff.piapi.core.MarketDataService
+import ru.tinkoff.piapi.core.exception.ApiRuntimeException
 
 import java.time.Instant
+import scala.annotation.tailrec
 import scala.jdk.CollectionConverters.CollectionHasAsScala
+import scala.util.{Failure, Success, Try}
 
 object util {
+  val log: Logger = Logger(getClass.getName.stripSuffix("$"))
+
+  @tailrec
   def getCandles(shareWrapper: ShareWrapper,
                  from: Instant,
                  to: Instant,
                  interval: CandleInterval)
-                (implicit marketDataService: MarketDataService): List[HistoricCandle] = {
-    marketDataService
-      .getCandlesSync(shareWrapper.figi, from, to, interval)
-      .asScala
-      .toList
+                (implicit config: Config,
+                 marketDataService: MarketDataService): List[HistoricCandle] = {
+
+    Try {
+      marketDataService
+        .getCandlesSync(shareWrapper.figi, from, to, interval)
+        .asScala
+        .toList
+    } match {
+      case Success(candles) => candles
+      case Failure(exception: ApiRuntimeException) =>
+        log.error(exception.toString)
+        Thread.sleep(config.tinkoffInvestApi.rateLimitPauseMillis)
+        getCandles(shareWrapper, from, to, interval)
+      case Failure(exception) =>
+        log.error(exception.toString)
+        throw exception
+    }
   }
 }
