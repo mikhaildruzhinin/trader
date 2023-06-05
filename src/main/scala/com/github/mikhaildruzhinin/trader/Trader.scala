63f2258a103245c0716bc28faf4d00cccb11f7fc
Mikhail Druzhinin <druzhininmy@gmail.com>
2023-06-06 00:53:54 +0300

    add TaskManager

--- src/main/scala/com/github/mikhaildruzhinin/trader/Trader.scala
+++ src/main/scala/com/github/mikhaildruzhinin/trader/Trader.scala
@@ -1,14 +1,12 @@
 package com.github.mikhaildruzhinin.trader
 
 import com.github.kagkarlsson.scheduler.Scheduler
-import com.github.kagkarlsson.scheduler.task.helper.Tasks
-import com.github.kagkarlsson.scheduler.task.schedule.Schedules
 import com.github.mikhaildruzhinin.trader.client._
 import com.github.mikhaildruzhinin.trader.config.{AppConfig, ConfigReader}
-import com.github.mikhaildruzhinin.trader.core.handlers._
+import com.github.mikhaildruzhinin.trader.core.TaskManager
 import com.typesafe.scalalogging.Logger
 
-import java.time.{Instant, ZoneId}
+import java.time.Instant
 
 object Trader extends App {
   val log: Logger = Logger(getClass.getName.stripSuffix("$"))
@@ -16,43 +14,16 @@ object Trader extends App {
   implicit val appConfig: AppConfig = ConfigReader.appConfig
   implicit val investApiClient: BaseInvestApiClient = SyncInvestApiClient
 
-    val startUpTask = Tasks.oneTime("start-up")
-      .execute(StartUpHandler())
-
-  val purchaseTask = Tasks.recurring(
-    "purchase",
-    Schedules.cron(
-      "0 0 9 * * *",
-      ZoneId.of("UTC")
-    )
-  ).execute(PurchaseHandler())
-
-  val monitorTask = Tasks.recurring(
-    "monitor",
-    Schedules.cron(
-      "0 */2 9-12 * * *",
-      ZoneId.of("UTC")
-    )
-  ).execute(MonitorHandler())
-
-  val sellTask = Tasks.recurring(
-    "sell",
-    Schedules.cron(
-      "0 0 13 * * *",
-      ZoneId.of("UTC")
-    )
-  ).execute(SellHandler())
-
   val scheduler: Scheduler = Scheduler
     .create(
       appConfig.slick.db.properties.dataSource,
-      startUpTask
+      TaskManager.startUpTask
     )
     .tableName(appConfig.scheduler.tableName)
     .startTasks(
-      purchaseTask,
-      monitorTask,
-      sellTask
+      TaskManager.purchaseTask,
+      TaskManager.monitorTask,
+      TaskManager.sellTask
     )
     .threads(appConfig.scheduler.numThreads)
     .registerShutdownHook()
@@ -62,7 +33,7 @@ object Trader extends App {
 
   scheduler
     .schedule(
-      startUpTask.instance("1"),
+      TaskManager.startUpTask.instance("1"),
       Instant.now.plusSeconds(5)
     )
 }
---
+++ src/main/scala/com/github/mikhaildruzhinin/trader/core/TaskManager.scala
@@ -0,0 +1,41 @@
+package com.github.mikhaildruzhinin.trader.core
+
+import com.github.kagkarlsson.scheduler.task.helper.{OneTimeTask, RecurringTask, Tasks}
+import com.github.kagkarlsson.scheduler.task.schedule.Schedules
+import com.github.mikhaildruzhinin.trader.client._
+import com.github.mikhaildruzhinin.trader.config.{AppConfig, ConfigReader}
+import com.github.mikhaildruzhinin.trader.core.handlers._
+
+import java.time.ZoneId
+
+object TaskManager {
+  implicit val appConfig: AppConfig = ConfigReader.appConfig
+  implicit val investApiClient: BaseInvestApiClient = SyncInvestApiClient
+
+  val startUpTask: OneTimeTask[Void] = Tasks.oneTime("start-up")
+    .execute(StartUpHandler())
+
+  val purchaseTask: RecurringTask[Void] = Tasks.recurring(
+    "purchase",
+    Schedules.cron(
+      "0 0 9 * * *",
+      ZoneId.of("UTC")
+    )
+  ).execute(PurchaseHandler())
+
+  val monitorTask: RecurringTask[Void] = Tasks.recurring(
+    "monitor",
+    Schedules.cron(
+      "0 */2 9-12 * * *",
+      ZoneId.of("UTC")
+    )
+  ).execute(MonitorHandler())
+
+  val sellTask: RecurringTask[Void] = Tasks.recurring(
+    "sell",
+    Schedules.cron(
+      "0 0 13 * * *",
+      ZoneId.of("UTC")
+    )
+  ).execute(SellHandler())
+}
