4cdef688ee272889d6db350d0aae73bea91deedb
Mikhail Druzhinin <druzhininmy@gmail.com>
2023-08-27 19:25:00 +0300

    update Purchase and Monitor

--- src/main/scala/ru/mikhaildruzhinin/trader/core/Monitor.scala
+++ src/main/scala/ru/mikhaildruzhinin/trader/core/Monitor.scala
@@ -9,7 +9,7 @@ import scala.concurrent.Future
 object Monitor {
   val log: Logger = Logger(getClass.getName)
 
-  def apply(services: Services): Future[Unit] = for {
+  def apply(services: Services): Future[Int] = for {
     shares <- services.shareService.getPersistedShares(TypeCode.Purchased)
     prices <- services.priceService.getCurrentPrices(shares)
     updatedShares <- services.shareService.updateCurrentPrices(shares, prices)
@@ -18,5 +18,5 @@ object Monitor {
     soldSharesNum <- services.shareService.persistUpdatedShares(sell.map(_._1), TypeCode.Sold)
     _ <- Future(log.info(s"Sold: ${soldSharesNum.sum}"))
     _ <- services.shareService.persistUpdatedShares(keep.map(_._1), TypeCode.Purchased)
-  } yield ()
+  } yield soldSharesNum.sum
 }
--- src/main/scala/ru/mikhaildruzhinin/trader/core/Purchase.scala
+++ src/main/scala/ru/mikhaildruzhinin/trader/core/Purchase.scala
@@ -10,44 +10,44 @@ import scala.concurrent.Future
 object Purchase {
   val log: Logger = Logger(getClass.getName)
 
-  private def getAvailableShares(services: Services): Future[Seq[ShareWrapper]] = for {
+  private def persistAvailableShares(services: Services): Future[(Seq[ShareWrapper], Int)] = for {
 
     shares <- services.shareService.getAvailableShares
     candles <- services.historicCandleService.getWrappedCandles(shares)
     availableShares <- services.shareService.getUpdatedShares(shares, candles)
     numAvailableShares <- services.shareService.persistNewShares(availableShares, TypeCode.Available)
     _ <- Future(log.info(s"Available: ${numAvailableShares.getOrElse(0)}"))
-  } yield availableShares
+  } yield (availableShares, numAvailableShares.getOrElse(0))
 
-  private def getUpdatedShares(services: Services,
-                               availableShares: Seq[ShareWrapper]): Future[Seq[ShareWrapper]] = for {
+  private def persistUpdatedShares(services: Services,
+                                   availableShares: Seq[ShareWrapper]): Future[(Seq[ShareWrapper], Int)] = for {
 
     currentPrices <- services.priceService.getCurrentPrices(availableShares)
     updatedShares <- services.shareService.updateCurrentPrices(availableShares, currentPrices)
     numUpdatedShares <- services.shareService.persistUpdatedShares(updatedShares, TypeCode.Available)
     _ <- Future(log.info(s"Updated: ${numUpdatedShares.sum}"))
-  } yield updatedShares
+  } yield (updatedShares, numUpdatedShares.sum)
 
-  private def getUptrendShares(services: Services,
-                               updatedShares: Seq[ShareWrapper]): Future[Seq[ShareWrapper]] = for {
+  private def persistUptrendShares(services: Services,
+                                   updatedShares: Seq[ShareWrapper]): Future[(Seq[ShareWrapper], Int)] = for {
     uptrendShares <- services.shareService.filterUptrend(updatedShares)
     numUptrendShares <- services.shareService.persistUpdatedShares(uptrendShares, TypeCode.Uptrend)
     _ <- Future(log.info(s"Best uptrend: ${numUptrendShares.sum}"))
-  } yield uptrendShares
+  } yield (uptrendShares, numUptrendShares.sum)
 
-  private def getPurchasedShares(services: Services,
-                                 uptrendShares: Seq[ShareWrapper]): Future[Seq[ShareWrapper]] = for {
+  private def persistPurchasedShares(services: Services,
+                                     uptrendShares: Seq[ShareWrapper]): Future[(Seq[ShareWrapper], Int)] = for {
     account <- services.accountService.getAccount
     mockPurchasePrices <- Future(uptrendShares.map(_.currentPrice))
     purchasedShares <- services.shareService.updatePurchasePrices(uptrendShares, mockPurchasePrices)
     numPurchasedShares <- services.shareService.persistUpdatedShares(purchasedShares, TypeCode.Purchased)
     _ <- Future(log.info(s"Purchased: ${numPurchasedShares.sum}"))
-  } yield purchasedShares
+  } yield (purchasedShares, numPurchasedShares.sum)
 
   def apply(services: Services): Future[Unit] = for {
-    availableShares <- getAvailableShares(services)
-    updatedShares <- getUpdatedShares(services, availableShares)
-    uptrendShares <- getUptrendShares(services, updatedShares)
-    purchasedShares <- getPurchasedShares(services, uptrendShares)
-  } yield ()
+    (availableShares, _) <- persistAvailableShares(services)
+    (updatedShares, _) <- persistUpdatedShares(services, availableShares)
+    (uptrendShares, _) <- persistUptrendShares(services, updatedShares)
+    (purchasedShares, numPurchasedShares) <- persistPurchasedShares(services, uptrendShares)
+  } yield numPurchasedShares
 }
