528293969cb182fc124feb10708027439821ae3a
Mikhail Druzhinin <druzhininmy@gmail.com>
2023-10-03 12:04:29 +0300

    add Spring app

---
+++ src/test/java/ru/mikhaildruzhinin/trader/TestTraderApplication.java
@@ -0,0 +1,23 @@
+package ru.mikhaildruzhinin.trader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class TestTraderApplication {
+
+	@Bean
+	@ServiceConnection
+	PostgreSQLContainer<?> postgresContainer() {
+		return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.from(TraderApplication::main).with(TestTraderApplication.class).run(args);
+	}
+
+}
---
+++ src/test/java/ru/mikhaildruzhinin/trader/TraderApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mikhaildruzhinin.trader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
