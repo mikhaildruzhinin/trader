0740e499e007fdd4eda75d1dbb362aa8ed78bc5c
Mikhail Druzhinin <druzhininmy@gmail.com>
2023-11-25 03:17:02 +0300

    move TradingStrategy

--- src/test/scala/ru/mikhaildruzhinin/trader/Tests.scala
+++ src/test/scala/ru/mikhaildruzhinin/trader/Tests.scala
@@ -1,12 +1,7 @@
 package ru.mikhaildruzhinin.trader
 
 import org.scalatest.funsuite.AnyFunSuite
-import org.ta4j.core._
-import org.ta4j.core.criteria.pnl.GrossReturnCriterion
-import org.ta4j.core.indicators.EMAIndicator
-import org.ta4j.core.indicators.helpers.ClosePriceIndicator
-import org.ta4j.core.num.{DecimalNum, Num}
-import org.ta4j.core.rules._
+import org.ta4j.core.BaseBarSeriesBuilder
 import pureconfig.ConfigReader.Result
 import pureconfig.error.ConfigReaderFailures
 import pureconfig.generic.ProductHint
@@ -83,37 +78,4 @@ class Tests extends AnyFunSuite {
       }
     )
   }
-}
-
-trait TradingStrategy {
-  def apply(barSeries: BarSeries): Future[TradingStrategyResult]
-}
-
-object EmaCrossoverStrategy extends TradingStrategy {
-  override def apply(barSeries: BarSeries): Future[TradingStrategyResult] = Future {
-    val closePriceIndicator = new ClosePriceIndicator(barSeries)
-    val shortEma = new EMAIndicator(closePriceIndicator, 20)
-    val longEma = new EMAIndicator(closePriceIndicator, 50)
-
-    val entryRule = new CrossedUpIndicatorRule(shortEma, longEma)
-
-    val exitRule = new CrossedDownIndicatorRule(shortEma, longEma)
-      .or(new StopGainRule(closePriceIndicator, 2))
-      .or(new StopLossRule(closePriceIndicator, 3))
-
-    val strategy: Strategy = new BaseStrategy(entryRule, exitRule)
-
-    val seriesManager = new BarSeriesManager(barSeries)
-    val tradingRecord: TradingRecord = seriesManager.run(strategy)
-
-    val criterion = new GrossReturnCriterion()
-    val criterionValue: Num = criterion.calculate(barSeries, tradingRecord)
-      .multipliedBy(DecimalNum.valueOf(100))
-    TradingStrategyResult(tradingRecord, criterionValue)
-  }
-}
-
-case class TradingStrategyResult(tradingRecord: TradingRecord,
-                                 criterionValue: Num) {
-  override def toString: String = super.toString // TODO: override
 }
